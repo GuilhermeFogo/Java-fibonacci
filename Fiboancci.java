a900bd37916247657bf681775d8ec7e4028466f4
Guilherme Fogo <gui.fogo17@gmail.com>
2016-10-07 10:52:44 -0300

    Update Fiboancci.java

--- Fiboancci.java
+++ Fiboancci.java
@@ -3,12 +3,12 @@ public class fibonaci {
 	
 	public static void main (String[] args){
 	
-	int num,num1,z;
+	int num,num1,z;// criação de 3 variaveis 
 	num=0;
 	num1=1;
 	
-		System.out.print("0 1");
-			for(int i= 0; i<50; i++){
+		System.out.print("0 1");// impressao de 0 e 1 na tela
+			for(int i= 0; i<50; i++){ //o for controla a quantidade de números a serem exibidos o exemplo foi de 50
 				System.out.print(" " + (num+num1));
 				z=num;
 				num=num1;
